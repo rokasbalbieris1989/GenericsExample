db24719c0359aa75285467383b3e731d84154a37
Rokas Balbieris <rokas.balbieris@yahoo.com>
2019-09-29 07:41:44 +0300

    created a class with generic tipes

--- src/genericsexample/GenericsExample.java
+++ src/genericsexample/GenericsExample.java
@@ -10,6 +10,7 @@ public class GenericsExample {
     
     public static void main(String[] args) {
         
+        Container<Integer,String> container1 = new Container(12,"Red");
     }
     
 }
